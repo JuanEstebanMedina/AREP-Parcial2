879b0abc515c2d37b2e18927b0b0372c7f42e1e0
JuanEstebanMedina <juan.medina-r@mail.escuelaing.edu.co>
2025-10-22 08:53:43 -0500

    Initial Commit

---
+++ mathservices/src/main/java/co/edu/escuelaing/mathservices/MathservicesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.mathservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathservicesApplication.class, args);
+	}
+
+}
---
+++ mathservices/src/test/java/co/edu/escuelaing/mathservices/MathservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.mathservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
