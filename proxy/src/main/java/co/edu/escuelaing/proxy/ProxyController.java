b253cd91d7c1b3832f801679ecb54ce0afd7bb35
JuanEstebanMedina <juan.medina-r@mail.escuelaing.edu.co>
2025-10-22 11:01:53 -0500

    chore: change urls

--- proxy/src/main/java/co/edu/escuelaing/proxy/ProxyController.java
+++ proxy/src/main/java/co/edu/escuelaing/proxy/ProxyController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProxyController {
 
-    private static final String URL1 = "http://localhost:8081/catalan";
-    private static final String URL2 = "http://localhost:8082/catalan";
+    // private static final String URL1 = "http://localhost:8081/catalan";
+    // private static final String URL2 = "http://localhost:8082/catalan";
 
     // EC2 instances
-    // private static final String URL1 = "http://35.175.219.160:8081/catalan";
-    // private static final String URL2 = "http://52.90.160.34:8081/catalan";
+    private static final String URL1 = "http://35.175.219.160:8081/catalan";
+    private static final String URL2 = "http://52.90.160.34:8081/catalan";
 
     @GetMapping("/catalan")
     public String catalan(@RequestParam(defaultValue = "10") String value) throws IOException {
